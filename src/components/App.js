815eebc3dcc9fc7c089d4fe71af1b6781c7a5998
Daniel Merys <dkmerys@gmail.com>
2020-07-24 10:16:41 -0700

    update MapControl to change currently visible state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import MapControl from './MapControl';
 
--- src/components/MapControl.js
+++ src/components/MapControl.js
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { withFirestore, isLoaded } from 'react-redux-firebase';
+import { useSelector } from 'react-redux'
+import firebase from '../firebase'
 import * as a from './../actions';
 
-class MapControl extends React.Component {
-  constructor(props)
+function MapControl(){
+  let currentlyVisibleState = null
+  let buttonText = null
+
+  const [visibleForm, setForm] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  function returnToGallery(){
+    if (selectedImage !== null) {
+      setSelectedImage(null);
+      setEditing(false);
+      setForm(false);
+    } else {
+      setForm(!visibleForm);
+    }
+  }
+
+  const user = firebase.auth().currentUser;
+
+  // if (user) {
+    if (editing === true){
+      currentlyVisibleState = <EditImage
+                              editing={editing}
+                              setEditing={setEditing}
+                              visibleForm={visibleForm}
+                              setForm={setForm}
+                              selectedImage={selectedImage} />
+      buttonText = "Return To Gallery"
+    } else if (selectedImage !== null) {
+      currentlyVisibleState = <ImageDetail
+                              setSelectedImage={setSelectedImage}
+                              selectedImage={selectedImage}
+                              editing={editing}
+                              setEditing={setEditing} />
+      buttonText = "Return To Gallery"
+    } else if (visibleForm === true) {
+      currentlyVisibleState = <NewImageForm
+                              setForm={setForm}
+                              visibleForm={visibleForm} />
+      buttonText = "View Images"
+    } else {
+      currentlyVisibleState = <ImageList
+                              setSelectedImage={setSelectedImage}
+                              selectedImage={selectedImage} />
+      buttonText = "Add Image"
+    }
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={ () => returnToGallery() }>{buttonText}</button>
+      </React.Fragment>
+    )
+  // } else {
+  //   return (
+  //     <div>
+  //       <p>Please Sign In</p>
+  //     </div>
+  //   )
+  // }
+
 }
+
+export default withFirestore(MapControl);
