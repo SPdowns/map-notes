c61de7539261c0c10a18ab3bfd0a68995af454e8
Daniel Merys <dkmerys@gmail.com>
2020-07-24 11:58:21 -0700

    add image detail component

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { withFirestore, isLoaded, useFirestore, useFirestoreConnect } from 'react-redux-firebase';
+import 'firebase/firestore';
+
+function ImageDetail(props) {
+  const { setSelectedImage, selectedImage, editing, setEditing } = props
+  console.log(selectedImage)
+  const firestore = useFirestore();
+  useFirestoreConnect([
+    {
+      collection: 'images',
+      doc: selectedImage
+    }
+  ]);
+
+  const handleDeletingImage = (selectedImage) => {
+    firestore.delete({collection:
+    'images',
+    doc: selectedImage});
+    setSelectedImage(null)
+  }
+
+  const image=useSelector(
+    ({ firestore: { data } }) => data.images && data.images(selectedImage)
+  )
+  return(
+    <React.Fragment>
+      <h1>Test</h1>
+
+      <button onClick={ () => setEditing(!editing)}>Edit Image</button>
+      <button onClick={ () => handleDeletingImage(selectedImage)}>Delete Image</button>
+    </React.Fragment>
+  );
+}
+
+ImageDetail.propTypes = {
+  image: PropTypes.object
+}
+
+export default ImageDetail
