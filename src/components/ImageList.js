ac2ed618e0261364607a5c029c21dbebd8db7775
Daniel Merys <dkmerys@gmail.com>
2020-07-24 13:14:25 -0700

    add functionality to ImageList component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,33 @@
-function ImageList() {
-  return null
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import Image from './Image';
+
+function ImageList(props) {
+  useFirestoreConnect([
+    {collection: 'images'}
+  ]);
+  const images = useSelector(state => state.firestore.ordered.images);
+  if(isLoaded(images)){
+    const {setSelectedImage, selectedImage} = props
+    return(
+      <React.Fragment>
+        {images.map((image) => {
+          return <Image
+          whenImageClicked = {setSelectedImage}
+          imageName={image.imageName}
+          id={image.id}/>
+        })}
+      </React.Fragment>
+    )
+  } else {
+    return(
+      <React.Fragment>
+        <h3>Loading...</h3>
+      </React.Fragment>
+    )
+  }
 }
 
 export default ImageList
