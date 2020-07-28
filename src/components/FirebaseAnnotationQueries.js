e669bebd5bab589b95ff3e4918042fba05a6b782
Daniel Merys <dkmerys@gmail.com>
2020-07-28 15:26:40 -0700

    rename FirebaseAnnotationQueries

---
+++ src/components/FirebaseAnnotationQueries.js
@@ -0,0 +1,10 @@
+  // annotations_table
+  // geometry, data, image
+
+// export const saveAnnotationToFirebase(annotation, image) => {
+//   const annotations = []
+//   return annotations
+// }
+// export const getAnnotationsFromFirebase(image) => {
+
+// }
--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -3,52 +3,53 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import { withFirestore, isLoaded, useFirestore, useFirestoreConnect } from 'react-redux-firebase';
 import 'firebase/firestore';
-import { getAnnotationsFromFirebase, saveAnnotationToStore } from './firebase_annotation_queries'
+import { getAnnotationsFromFirebase, saveAnnotationToFirebase } from './FirebaseAnnotationQueries'
 
 function ImageDetail(props) {
   const { setSelectedImage, selectedImage, editing, setEditing } = props
   console.log(selectedImage)
   const firestore = useFirestore();
-  // const [annotations, setAnnotations] = useState(getAnnotationsFromFirebase(imageId));
+  // const [annotations, setAnnotations] = useState(getAnnotationsFromFirebase(image));
   // const [annotation, setAnnotation] = useState({})
-
+  
   useFirestoreConnect([
     {
       collection: 'images',
       doc: selectedImage
     }
   ]);
-
+  
   // onChangeAnnotation = (annotation) => {
-  //   setAnnotation({ annotation })
-  // }
-
-  // onCreateAnnotation = (annotation) => {
-  //   const { geometry, data } = annotation
-  //   const  { imageId } =  selectedImage
-
-  //   saveAnnotationToStore(annotation, imageId)
-
-
-  // }
-
-
-
-  const handleDeletingImage = (selectedImage) => {
-    firestore.delete({collection:
-    'images',
-    doc: selectedImage});
-    setSelectedImage(null)
-  }
-
-  const image = useSelector(
-    ({ firestore: { data } }) => data.images && data.images[selectedImage]
-  )
+  //     setAnnotation({ annotation })
+  //   }
+    
+  //   onCreateAnnotation = (annotation) => {
+  //       const { geometry, data } = annotation
+  //       const  { image } =  selectedImage
+      
+  //       saveAnnotationToFirebase(annotation, image)
+      
+      
+  //     }
+      
+      
+      
+      const handleDeletingImage = (selectedImage) => {
+        firestore.delete({collection:
+          'images',
+          doc: selectedImage});
+          setSelectedImage(null)
+        }
+        
+        const image = useSelector(
+          ({ firestore: { data } }) => data.images && data.images[selectedImage]
+          )
+          console.log(image)
 
   return(
     <Fragment>
       <h1>{image.imageName}</h1>
-      <img src={image.imageURL} />
+      <img width='1000' height='1000' src={image.imageURL} />
       <button onClick={ () => setEditing(!editing)}>Edit Image</button>
       <button onClick={ () => handleDeletingImage(selectedImage)}>Delete Image</button>
     </Fragment>
